febde2108bd0c74ebf41e380df9093c797f2cd40
Fale1278 <kingnanmuafale@gmail.com>
2026-01-15 06:40:18 -0800

    Updated Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,32 @@
+import myBlogImg from "../assets/MyBlog.png"
+import TKF from "../assets/TKF.png"
+
 export const projects = [
   {
     id: 1,
-    title: 'E-commerce Platform',
+    title: 'Personal Blog Website',
     slug: 'ecommerce-platform',
     description:
-      'A scalable e-commerce experience built with modern React patterns, focusing on performance and maintainability.',
+      'A scalable blogging experience built with modern React patterns, focusing on performance and maintainability.',
     longDescription:
       'This project explores scalable frontend architecture for e-commerce applications, including reusable components, optimized rendering, and responsive layouts.',
     tags: ['React', 'Redux', 'Node.js'],
-    image: 'hero.jpg',
-    live: '#',
-    github: '#',
+    image: myBlogImg ,
+    live: 'https://my-personal-blog-gamma.vercel.app/',
+    github: 'https://github.com/Fale1278/MyPersonalBlog',
   },
   {
     id: 2,
-    title: 'Personal Dashboard',
+    title: 'Tech Organisation PLatform',
     slug: 'personal-dashboard',
     description:
       'A productivity dashboard leveraging browser storage and advanced React hooks.',
     longDescription:
       'Built to demonstrate effective state management, data persistence, and UI clarity in complex user interfaces.',
     tags: ['React Hooks', 'CSS Grid', 'LocalStorage'],
-    image: '/images/projects/dashboard.jpg',
-    live: '#',
-    github: '#',
+    image: TKF,
+    live: 'https://fale1278.github.io/TKF-THE-KINGSHIMA-FOUNDATION/',
+    github: 'https://github.com/Fale1278/TKF-THE-KINGSHIMA-FOUNDATION',
   },
   {
     id: 3,
